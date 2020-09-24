54d6bebf216c1ea7a1a22d3f3e86c1a993c96844
dean <jhlee@lunasoft.co.kr>
2020-09-24 17:11:40 +0900

    add test log

--- src/test/java/com/rabbitmq/sample/SampleApplicationTests.java
+++ src/test/java/com/rabbitmq/sample/SampleApplicationTests.java
@@ -1,13 +1,16 @@
 package com.rabbitmq.sample;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Slf4j
 @SpringBootTest
 class SampleApplicationTests {
 
     @Test
     void contextLoads() {
+        log.info("zzz : test");
     }
 
 }
